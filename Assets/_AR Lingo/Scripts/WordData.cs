c37eb430d980cf141803daf56c495d5a7391f279
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2025-12-10 15:23:42 +0700

    second commit

---
+++ Assets/_AR Lingo/Scripts/WordData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WordData", menuName = "Scriptable Objects/WordData")]
+public class WordData : ScriptableObject
+{
+    
+}
---
+++ Assets/_AR Lingo/Scripts/WordDatabase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WordDatabase", menuName = "Scriptable Objects/WordDatabase")]
+public class WordDatabase : ScriptableObject
+{
+    
+}
