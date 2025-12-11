e451cd0da387fc182e7671aa5840e556d6a20099
dkozTA <anhthe45776@gmail.com>
2025-12-12 01:48:27 +0700

    add first UI: - Home screen with button - Other screens

---
+++ Assets/_AR Lingo/Scripts/Controllers/SimpleMenuController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleMenuController : MonoBehaviour
+{
+    [Header("Kéo các Panel vào đây")]
+    public GameObject homePanel;
+    public GameObject scanPanel;
+    public GameObject dictionaryPanel;
+    public GameObject quizPanel;
+    public GameObject settingPanel;
+
+    // --- HÀM CHO CÁC NÚT Ở MÀN HÌNH HOME ---
+
+    public void OpenScan()
+    {
+        homePanel.SetActive(false); // Tắt Home
+        scanPanel.SetActive(true);  // Bật Scan
+    }
+
+    public void OpenDictionary()
+    {
+        homePanel.SetActive(false);
+        dictionaryPanel.SetActive(true);
+    }
+
+    public void OpenQuiz()
+    {
+        homePanel.SetActive(false);
+        quizPanel.SetActive(true);
+    }
+
+    public void OpenSetting()
+    {
+        homePanel.SetActive(false);
+        settingPanel.SetActive(true);
+    }
+
+    public void QuitApp()
+    {
+        // Khi chạy thử trong Unity thì nó hiện dòng này
+        Debug.Log("Đã thoát ứng dụng!"); 
+        // Khi build ra điện thoại thật thì lệnh này mới chạy
+        Application.Quit(); 
+    }
+
+    // --- HÀM CHO NÚT BACK (Dùng chung cho tất cả) ---
+
+    public void BackToHome()
+    {
+        // Tắt hết tất cả các panel con đi cho chắc ăn
+        scanPanel.SetActive(false);
+        dictionaryPanel.SetActive(false);
+        quizPanel.SetActive(false);
+        settingPanel.SetActive(false);
+
+        // Bật lại Home
+        homePanel.SetActive(true);
+    }
+}
